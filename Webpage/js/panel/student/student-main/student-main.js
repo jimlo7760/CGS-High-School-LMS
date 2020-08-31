ca9effb65157d7a3ccaa155f7bb04b7118561f9b
LLuking_ <46153378+LLuking@users.noreply.github.com>
2020-08-31 09:15:16 +0800

    student-page

---
+++ Webpage/js/panel/student/student-main/student-main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $('.left-content-manu-nevi').mouseenter(function () {
+        $(this).animate({
+            backgroundColor: '#337681',
+        }, 'fast')
+    })
+    $('.left-content-manu-nevi').mouseleave(function () {
+        $(this).animate({
+            backgroundColor: 'transparent',
+        }, 'fast')
+    })
+})
