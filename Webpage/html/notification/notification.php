c22698060288655c090859a7a8842eda86e986d1
yanzhenlu <39715308+LouhYinzoen@users.noreply.github.com>
2021-09-14 17:14:22 +0800

    create php for notification page

---
+++ Webpage/html/notification/notification.php
@@ -0,0 +1,452 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Notification</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../../CSS/whole-common.css">
+    <link rel="stylesheet" type="text/css" href="../../CSS/panel/panel-common.css">
+    <link rel="stylesheet" type="text/css" href="../../CSS/panel/notification/notification.css">
+    <script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js">
+
+    </script>
+    <script src="//cdn.jsdelivr.net/jquery.color-animation/1/mainfile">
+
+    </script>
+    <script type="text/javascript" src="../../js/panel/panel-common.js">
+
+    </script>
+    <script type="text/javascript" src="../../js/notification/notification.js">
+
+    </script>
+</head>
+<body>
+
+<div class="teacher-information">
+    <input type="hidden" class="user-type" value="0">
+    <!--
+        input the type of teacher here
+        0: subject;
+        1: homeroom;
+        2: coordinator
+    -->
+
+    <input type="hidden" class="course-id" value="1">
+    <!--
+        Only echo when the teacher is subject teacher
+    -->
+    <input type="hidden" class="homeroom-id" value="2">
+    <!--
+        Only echo when the teacher is homeroom teacher
+    -->
+</div>
+<div class="all">
+    <div class="left-content" style="height: 700px;">
+        <div class="left-box">
+            <div class="left-content-title stm">
+                MYP Student Management System
+            </div>
+            <div class="left-content-navi">
+                <div class="left-content-navi-item">
+                    <div class="left-content-navi-title stb">
+                        SCHOOL YEAR
+                    </div>
+                    <div class="left-content-navi-content">
+                        <span class="material-icons left-content-navi-img initial_transform">
+                            keyboard_arrow_up
+                        </span>
+                    </div>
+                </div>
+                <div class="left-content-manu stb">
+                    <div class="left-content-manu-nevi" name="20201">
+                        <div class="left-content-manu-year">
+                            2019 - 2020
+                        </div>
+                        <div class="left-content-manu-current">
+                            CURRENT
+                        </div>
+                    </div>
+                    <div class="left-content-manu-nevi" name="20192">
+                        <div class="left-content-manu-year">
+                            2018 - 2019
+                        </div>
+                        <div class="left-content-manu-current no-select">
+                            CURRENT
+                        </div>
+                    </div>
+                    <div class="left-content-manu-nevi" name="20191">
+                        <div class="left-content-manu-year">
+                            2018 - 2019
+                        </div>
+                        <div class="left-content-manu-current no-select">
+                            CURRENT
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="left-box-add">
+            <div class="left-content-navi">
+                <div class="left-content-navi-item">
+                    <div class="left-content-navi-title stb">
+                        Chinese A1
+                    </div>
+                    <div class="left-content-navi-content">
+                        <span class="material-icons left-content-navi-img initial_transform">
+                            keyboard_arrow_up
+                        </span>
+                    </div>
+                </div>
+                <div class="left-content-manu navi-manage-student stb">
+                    <div class="left-content-manu-nevi" name="student" id="student-nevi"
+                         style="background-color: rgb(0, 60, 70);">
+                        <div class="left-content-manu-year">
+                            Students
+                        </div>
+                    </div>
+                    <div class="left-content-manu-nevi" name="exam" id="exam-nevi"
+                         style="background-color: rgba(255, 255, 255, 0);">
+                        <div class="left-content-manu-year">
+                            Exams &amp; Tests
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="right-content">
+        <div class="right-top">
+            <div class="right-top-left">
+                <div class="right-top-research">
+                    <span class="material-icons right-top-research-img">
+                        search
+                    </span>
+                    <input class="right-top-research-text pointer-alt stm" placeholder="Search by course name"
+                           type="text" maxlength="50">
+                </div>
+            </div>
+            <div class="right-top-right">
+                <i class="material-icons right-top-noti">
+                    notifications
+                </i>
+                <div class="right-top-noti-cir" style="top: 25px; left: 947.547px; opacity: 1;">
+
+                </div>
+                <div class="right-top-person">
+                    <div class="right-top-person-name stm">
+                        Danny Xu
+                    </div>
+                    <img src="../../img/图像 1@2x.png" height="45" width="45" class="right-top-person-portrait">
+                    <i class="material-icons right-top-person-arrow initial_transform">
+                        keyboard_arrow_down
+                    </i>
+                </div>
+            </div>
+        </div>
+        <div class="right-down">
+            <div class="right-info">
+                <div class="right-info-left stb">
+                    <div class="right-title" style="color: #343A3E">
+                        Notification
+                    </div>
+                </div>
+            </div>
+            <div class="right-person-info-row-box">
+                <div class="unread-notification">
+
+                </div>
+                <div class="right-person-info-row-box-title stm">
+                    $application_type
+                </div>
+                <div class="right-person-info-row-box-right">
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Student Name
+                        </div>
+                        <div class="right-person-info-row-box-des stm">
+                            $application_student_name
+                        </div>
+                    </div>
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Dated Received
+                        </div>
+                        <div class="right-person-info-row-box-des stm">
+                            $application_date
+                        </div>
+                    </div>
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Status
+                        </div>
+                        <div class="right-person-info-row-box-des stm" style="color: #FFC103">
+
+                        </div>
+                    </div>
+                    <input type="hidden" class="first-class" value="abcab">
+                    <input type="hidden" class="status-id" value="2">
+                    <input type="hidden" class="application-id" value="">
+                    <i class="material-icons right-person-info-row-box-img">
+                        chevron_right
+                    </i>
+                </div>
+            </div>
+            <div class="right-person-info-row-box">
+                <div class="unread-notification">
+
+                </div>
+                <div class="right-person-info-row-box-title stm">
+                    Delete Course
+                </div>
+                <div class="right-person-info-row-box-right">
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Student Name
+                        </div>
+                        <div class="right-person-info-row-box-des stm">
+                            abcde
+                        </div>
+                    </div>
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Dated Received
+                        </div>
+                        <div class="right-person-info-row-box-des stm">
+                            2019-03-31
+                        </div>
+                    </div>
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Status
+                        </div>
+                        <div class="right-person-info-row-box-des stm" style="color: #FFC103">
+                            Waiting for Approval
+                        </div>
+                    </div>
+                    <input type="hidden" class="first-class" value="abcab">
+                    <input type="hidden" class="status-id" value="1">
+                    <input type="hidden" class="application-id" value="">
+                    <i class="material-icons right-person-info-row-box-img">
+                        chevron_right
+                    </i>
+                </div>
+            </div>
+            <div class="right-person-info-row-box">
+                <div class="unread-notification">
+
+                </div>
+                <div class="right-person-info-row-box-title stm">
+                    Swap Course
+                </div>
+                <div class="right-person-info-row-box-right">
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Student Name
+                        </div>
+                        <div class="right-person-info-row-box-des stm">
+                            abcde
+                        </div>
+                    </div>
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Dated Received
+                        </div>
+                        <div class="right-person-info-row-box-des stm">
+                            2019-03-31
+                        </div>
+                    </div>
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Status
+                        </div>
+                        <div class="right-person-info-row-box-des stm" style="color: #FFC103">
+                            Waiting for Approval
+                        </div>
+                    </div>
+                    <input type="hidden" class="first-class" value="abcab">
+                    <input type="hidden" class="second-class" value="edcbd">
+                    <input type="hidden" class="status-id" value="3">
+                    <input type="hidden" class="application-id" value="">
+                    <i class="material-icons right-person-info-row-box-img">
+                        chevron_right
+                    </i>
+                </div>
+            </div>
+
+        </div>
+        <div class="right-down">
+            <div class="right-info">
+                <div class="right-info-left stb">
+                    <div class="right-title" style="color: #343A3E; font-size: 25px">
+                        Previous Tickets
+                    </div>
+                </div>
+            </div>
+            <div class="right-person-info-row-box">
+                <div class="right-person-info-row-box-title stm">
+                    Swap Course
+                </div>
+                <div class="right-person-info-row-box-right">
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Student Name
+                        </div>
+                        <div class="right-person-info-row-box-des stm">
+                            abcde
+                        </div>
+                    </div>
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Dated Received
+                        </div>
+                        <div class="right-person-info-row-box-des stm">
+                            2019-03-31
+                        </div>
+                    </div>
+                    <div class="right-person-info-row-box-text">
+                        <div class="right-person-info-row-box-subtitle str">
+                            Status
+                        </div>
+                        <div class="right-person-info-row-box-des stm" style="color: #29A745">
+                            Approved
+                        </div>
+                    </div>
+                    <input type="hidden" class="first-class" value="abcab">
+                    <input type="hidden" class="second-class" value="edcbd">
+                    <input type="hidden" class="homeroom-class-id" value="3">
+                    <input type="hidden" class="status-id" value="2">
+                    <input type="hidden" class="application-id" value="">
+                    <i class="material-icons right-person-info-row-box-img">
+                        chevron_right
+                    </i>
+                </div>
+            </div>
+        </div>
+        <div class="grey-bg">
+
+        </div>
+        <div class="personal-panel">
+            <div class="personal-panel-top">
+                <img src="../../img/图像 1@2x.png" height="30" width="30" class="personal-panel-portrait"/>
+                <div class="personal-panel-des">
+                    <div class="personal-panel-name stb">
+                        Danny Xu
+                    </div>
+                    <div class="personal-panel-mail str">
+                        dannyxu@163.com
+                    </div>
+                </div>
+            </div>
+            <div class="personal-panel-bottom">
+                <div class="personal-panel-row str">
+                    My Profile
+                </div>
+                <div class="personal-panel-row str">
+                    Add / Drop Class
+                </div>
+                <div class="personal-panel-row str">
+                    Goal Score
+                </div>
+                <div class="personal-panel-row-last str" onclick="window.location='../login/index.html'">
+                    Sign Out
+                </div>
+            </div>
+        </div>
+
+        <div class="result-box">
+            <ul class="result-box-row">
+                <li class="result-box-sort" value="class">
+                    Student
+                </li>
+                <li class="result-box-name">
+                    Danny Xu
+                </li>
+                <li class="result-box-year">
+                    0108706
+                </li>
+            </ul>
+            <div class="result-box-empty">
+                No result found containing 'Spainish'
+            </div>
+        </div>
+
+        <div class="share-box application-operation-box">
+            <form method="post" name="application_operation">
+                <div class="edit-box-upper">
+                    <div class="edit-box-top">
+                        <div class="edit-box-title stm">
+                            Ticket Details
+                        </div>
+                        <span class="material-icons edit-box-close">
+                            close
+                        </span>
+                    </div>
+                    <div class="edit-box-row">
+                        <div class="edit-box-innerbox ticket-detail-large str">
+                            <div class="edit-box-innerbox-title">
+                                Student Name
+                            </div>
+                            <div class="notification-box-display str">
+
+                            </div>
+                        </div>
+                        <div class=" edit-box-innerbox ticket-detail-small str">
+                            <div class="edit-box-innerbox-title">
+                                Date
+                            </div>
+                            <div class="notification-box-display str">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="class-adding-subtitle str" style="margin-top: 15px;">
+                        Added Course
+                    </div>
+                    <div class="adding-course-name stb">
+                        AAA
+                    </div>
+                    <div class="adding-course-name stb" style="display: none">
+                        BBB
+                    </div>
+                </div>
+                <div class="edit-box-downer">
+                    <div class="pending-box-innerbox">
+                        <div class="pending-box-innerbox-title str">
+                            Ticket Status
+                        </div>
+                        <div class="ticket-box-row">
+                            <span class="material-icons ticket-box-row-img">
+                                check_circle
+                            </span>
+                            <div class="ticket-box-row-text">
+                                Subject Teacher Approved
+                            </div>
+                        </div>
+                        <div class="ticket-box-row">
+                            <span class="material-icons ticket-box-row-img">
+                                check_circle
+                            </span>
+                            <div class="ticket-box-row-text">
+                                Homeroom Approved
+                            </div>
+                        </div>
+                        <div class="ticket-box-row">
+                            <span class="material-icons ticket-box-row-img">
+                                check_circle
+                            </span>
+                            <div class="ticket-box-row-text">
+                                Administrator Approved
+                            </div>
+                        </div>
+                    </div>
+                    <input type="hidden" class="application-id" value="">
+                    <input type="button" class="share-box-double-button edit-box-green" value="Approve" onclick="approveApplication()">
+                    <input type="button" class="share-box-double-button edit-box-red" value="Reject" onclick="rejectApplication()">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
